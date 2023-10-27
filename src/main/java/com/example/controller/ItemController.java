a5b49c50304f9b8da0d01d778d17efcda348a5e1
Morikawa Momoko <mmk.muraoka@gmail.com>
2023-10-27 09:49:37 +0900

    Lesson03実装完了

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -30,8 +30,7 @@ public class ItemController {
 	public String index(Model model) {
 		
 		List<Item> items = this.itemService.findAll();
-		
-		System.out.println(items.toString());
+		model.addAttribute("items", items);
 		return "item/index";
 	}
 	
@@ -40,17 +39,29 @@ public class ItemController {
 		return "item/torokuPage";
 	}
 	
+	@PostMapping("toroku")
+	public String toroku(ItemForm itemForm) {
+		this.itemService.save(itemForm);
+		return "redirect:/item";
+	}
+	
 	@GetMapping("henshu/{id}")
 	public String henshuPage(@PathVariable("id") Integer id, Model model
 			,@ModelAttribute("itemForm") ItemForm itemForm) {
+		Item item = this.itemService.findById(id);
+		itemForm.setName(item.getName());
+		itemForm.setPrice(item.getPrice());
+		model.addAttribute("id", id);
 		return "item/henshuPage";
 	}
 	@PostMapping("henshu/{id}")
 	public String henshu(@PathVariable("id") Integer id, @ModelAttribute("itemForm") ItemForm itemForm) {
+		this.itemService.update(id, itemForm);
 		return "redirect:/item";
 	}
 	@PostMapping("sakujo/{id}")
 	public String sakujo(@PathVariable("id") Integer id) {
+		this.itemService.delete(id);
 		return "redirect:/item";
 	}
 }
--- src/main/java/com/example/service/ItemService.java
+++ src/main/java/com/example/service/ItemService.java
@@ -1,11 +1,13 @@
 package com.example.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.entity.Item;
+import com.example.form.ItemForm;
 import com.example.repository.ItemRepository;
 
 @Service
@@ -20,5 +22,30 @@ public class ItemService {
 	
 	public List<Item> findAll() {
 		return this.itemRepository.findAll();
+	}
+	
+	public Item save(ItemForm itemForm) {
+		Item item = new Item();
+		
+		item.setName(itemForm.getName());
+		item.setPrice(itemForm.getPrice());
+		return this.itemRepository.save(item);
+	}
+	
+	public Item findById(Integer id) {
+		Optional<Item> optionalItem = this.itemRepository.findById(id);
+		Item item = optionalItem.get();
+		return item;
+	}
+	
+	public Item update(Integer id, ItemForm itemForm) {
+		Item item = this.findById(id);
+		item.setName(itemForm.getName());
+		item.setPrice(itemForm.getPrice());
+		return this.itemRepository.save(item);
+	}
+	
+	public void delete(Integer id) {
+		this.itemRepository.deleteById(id);
 	}
 }
